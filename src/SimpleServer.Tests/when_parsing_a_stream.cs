71ce9354ca8a5c4f7f516e45cfabef5c2df726a9
jmarnold <rncodepoet@gmail.com>
2011-09-14 01:36:31 -0500

    added tests for stream parser

---
+++ src/SimpleServer.Tests/when_parsing_a_stream.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace SimpleServer.Tests
+{
+    [TestFixture]
+    public class when_parsing_a_stream
+    {
+        [Test]
+        public void should_read_stream()
+        {
+            var message = Guid.NewGuid().ToString();
+            var stream = new MemoryStream();
+            writeAndPrepare(stream, message);
+
+            new StreamParser()
+                .Parse(stream)
+                .ShouldEqual(message);
+        }
+
+        private void writeAndPrepare(Stream stream, string message)
+        {
+            var writer = new StreamWriter(stream);
+            writer.Write(message);
+            writer.Flush();
+
+            stream.Seek(0, SeekOrigin.Begin);
+        }
+    }
+
+    public class StreamParser : IStreamParser
+    {
+        public string Parse(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
